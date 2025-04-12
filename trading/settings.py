85f0b374552ec0e0b6a1174554d44f6d25c42b63
tusharkoley <tusharkoley@Tushars-MacBook-Pro.local>
2025-04-12 14:10:04 +0530

    updated settings

--- trading/settings.py
+++ trading/settings.py
@@ -19,7 +19,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+# load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
