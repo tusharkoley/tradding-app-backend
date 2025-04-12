35a77574b98e09a1c5661325eb0d96a95f45e87b
tusharkoley <tusharkoley@Tushars-MacBook-Pro.local>
2025-04-12 12:41:16 +0530

    updated settings

--- trading/settings.py
+++ trading/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','localhost','1.0.0.127.in-addr.arpa','127.0.0.1','gentle-courage.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1:8000','localhost','1.0.0.127.in-addr.arpa','127.0.0.1','gentle-courage.up.railway.app','*']
 
 
 # Application definition
